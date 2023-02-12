dafdf9217db68f5e0716d065a34af8262eb2b253
em-vivek <vivek.yadav@eminds.ai>
2023-02-12 17:53:14 +0530

    saved and applied items-Done

--- src/user/controller.ts
+++ src/user/controller.ts
@@ -75,7 +75,7 @@ export const addUser = async (req: Request, res: Response, next: NextFunction) =
     }
 }
 
-export const SavedItems = async (req: Request, res: Response, next: NextFunction) => {
+export const savedItems = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const email = req.params.email
         const userDetails = await user.User.findOne({ email: email })
@@ -87,14 +87,14 @@ export const SavedItems = async (req: Request, res: Response, next: NextFunction
         if (!data) {
             res.locals = {
                 success: false,
-                message: "User profile could not be created"
+                message: "Failed to get saved items"
             }
             return next()
         }
         res.locals = {
             data: data,
             success: true,
-            message: "User has been created"
+            message: "Saved items has been retreived"
         }
         return next()
     }
@@ -108,7 +108,7 @@ export const SavedItems = async (req: Request, res: Response, next: NextFunction
     }
 }
 
-export const AppliedItems = async (req: Request, res: Response, next: NextFunction) => {
+export const appliedItems = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const email = req.params.email
         const userDetails = await user.User.findOne({ email: email })
@@ -120,14 +120,57 @@ export const AppliedItems = async (req: Request, res: Response, next: NextFuncti
         if (!data) {
             res.locals = {
                 success: false,
-                message: "User profile could not be created"
+                message: "Failed to get applied items"
             }
             return next()
         }
         res.locals = {
             data: data,
             success: true,
-            message: "User has been created"
+            message: "Applied items has been retreived"
+        }
+        return next()
+    }
+    catch (e) {
+        res.locals = {
+            success: false,
+            // path: "/user/profile",
+            message: e
+        }
+        return next()
+    }
+}
+
+
+export const items = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const email = req.params.email
+        const userDetails = await user.User.findOne({ email: email })
+        const savedJobs = await SavedJobs.find({ user_id: userDetails?.id })
+        const savedMentorship = await SavedMentorships.find({ user_id: userDetails?.id })
+        const savedScholarship = await SavedScholarships.find({ user_id: userDetails?.id })
+        const savedCourses = await SavedCourses.find({ user_id: userDetails?.id })
+        const appliedJobs = await AppliedJobs.find({ user_id: userDetails?.id })
+        const appliedMentorship = await AppliedMentorship.find({ user_id: userDetails?.id })
+        const appliedScholarship = await AppliedScholarships.find({ user_id: userDetails?.id })
+        const appliedCourses = await AppliedCourses.find({ user_id: userDetails?.id })
+        const data = {
+            jobs: { save: savedJobs, applied: appliedJobs },
+            courses: { save: savedCourses, applied: appliedCourses },
+            scholarship: { save: savedScholarship, applied: appliedScholarship },
+            mentorship: { save: savedMentorship, applied: appliedMentorship }
+        }
+        if (!data) {
+            res.locals = {
+                success: false,
+                message: "Failed to get items"
+            }
+            return next()
+        }
+        res.locals = {
+            data: data,
+            success: true,
+            message: "Items has been retreived"
         }
         return next()
     }
